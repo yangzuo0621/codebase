88faa9bd0759b628203988c21959a15ea85cc3ff
Zuo Yang <zuya@microsoft.com>
2021-06-11 12:25:39 +0800

    remove all files and prepare new folder structure

--- go/example/types.go
+++
@@ -1,6 +0,0 @@
-package example
-
-//go:generate mockgen -destination=./mock_$GOPACKAGE/types.go -package=mock_$GOPACKAGE github.com/yangzuo0621/codebase/go/example Foo
-type Foo interface {
-	Bar() (string, error)
-}
