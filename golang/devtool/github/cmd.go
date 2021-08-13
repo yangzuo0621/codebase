968d92d84956f5466e46443951a5735ffa4c47c4
Zuo Yang <zuya@microsoft.com>
2021-08-13 13:16:09 +0800

    add create github pull request command (#6)
    * add get command
    
    * create pr

--- golang/devtool/github/cmd.go
+++ golang/devtool/github/cmd.go
@@ -2,6 +2,7 @@ package github
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	hub "github.com/google/go-github/github"
@@ -15,6 +16,8 @@ func CreateCommand() *cobra.Command {
 		Use: "github",
 	}
 	c.AddCommand(createListCommand())
+	c.AddCommand(createGetCommand())
+	c.AddCommand(createPullRequestCommand())
 	return c
 }
 
@@ -37,7 +40,7 @@ func createListCommand() *cobra.Command {
 
 			repos, _, err := client.Repositories.List(ctx, "", nil)
 			if err != nil {
-				logger.Fatalf("list repo: %s", err)
+				logger.Fatalf("list repo failed: %s", err)
 			}
 
 			for _, repo := range repos {
@@ -46,5 +49,97 @@ func createListCommand() *cobra.Command {
 			return nil
 		},
 	}
+	return c
+}
+
+func createGetCommand() *cobra.Command {
+	var (
+		flagRepository string
+		flagOwner      string
+	)
+	c := &cobra.Command{
+		Use: "get",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			logger := logrus.New()
+			pat := os.Getenv("PERSONAL_ACCESS_TOKEN")
+			if pat == "" {
+				logger.Fatal("PERSONAL_ACCESS_TOKEN is empty")
+			}
+
+			ctx := context.Background()
+			ts := oauth2.StaticTokenSource(&oauth2.Token{
+				AccessToken: pat,
+			})
+			tc := oauth2.NewClient(ctx, ts)
+			client := hub.NewClient(tc)
+
+			repo, _, err := client.Repositories.Get(ctx, flagOwner, flagRepository)
+			if err != nil {
+				logger.Fatalf("get repo %s failed: %s", flagRepository, err)
+			}
+
+			fmt.Fprintf(cmd.OutOrStdout(), "ID= %d Name=%s\n", *repo.ID, *repo.Name)
+			return nil
+		},
+	}
+
+	c.Flags().StringVar(&flagRepository, "repository", "", "repository name")
+	c.Flags().StringVar(&flagOwner, "owner", "", "owner name")
+	c.MarkFlagRequired("repository")
+	c.MarkFlagRequired("owner")
+	return c
+}
+
+// https://docs.github.com/en/rest/reference/pulls#create-a-pull-request
+func createPullRequestCommand() *cobra.Command {
+	var (
+		flagRepository string
+		flagOwner      string
+		flagBase       string
+		flagHead       string
+		flagTitle      string
+		flagBody       string
+	)
+	c := &cobra.Command{
+		Use: "pr-create",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			logger := logrus.New()
+			pat := os.Getenv("PERSONAL_ACCESS_TOKEN")
+			if pat == "" {
+				logger.Fatal("PERSONAL_ACCESS_TOKEN is empty")
+			}
+
+			ctx := context.Background()
+			ts := oauth2.StaticTokenSource(&oauth2.Token{
+				AccessToken: pat,
+			})
+			tc := oauth2.NewClient(ctx, ts)
+			client := hub.NewClient(tc)
+
+			pr, _, err := client.PullRequests.Create(ctx, flagOwner, flagRepository, &hub.NewPullRequest{
+				Title: &flagTitle,
+				Head:  &flagHead,
+				Base:  &flagBase,
+				Body:  &flagBody,
+			})
+			if err != nil {
+				logger.Fatalf("create PR failed: %s", err)
+			}
+
+			fmt.Fprintf(cmd.OutOrStdout(), "ID= %d Number=%d\n", *pr.ID, *pr.Number)
+			return nil
+		},
+	}
+
+	c.Flags().StringVar(&flagRepository, "repository", "", "repository name")
+	c.Flags().StringVar(&flagOwner, "owner", "", "owner name")
+	c.Flags().StringVar(&flagBase, "base", "", "The name of the branch you want the changes pulled into.")
+	c.Flags().StringVar(&flagHead, "head", "", "The name of the branch where your changes are implemented.")
+	c.Flags().StringVar(&flagTitle, "title", "", "The name of pull request.")
+	c.Flags().StringVar(&flagBody, "body", "", "The body of pull request.")
+	c.MarkFlagRequired("repository")
+	c.MarkFlagRequired("owner")
+	c.MarkFlagRequired("base")
+	c.MarkFlagRequired("head")
 	return c
 }
